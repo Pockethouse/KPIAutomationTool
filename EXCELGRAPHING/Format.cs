58e0e12087e1f27bf1e33f8e450e94c8385e28fa
Pockethouse <101947734+Pockethouse@users.noreply.github.com>
2024-08-02 18:31:53 -0400

    Added functionality to make sure data can be appended without being overwritten

--- EXCELGRAPHING/Format.cs
+++ EXCELGRAPHING/Format.cs
@@ -19,92 +19,97 @@ public class Format
 
     }
 
-    public void AddResultsToSheet<T1, T2>(IEnumerable<T1> results1, IEnumerable<T2> results2, string sheetName)
-    {
-        using (var package = new ExcelPackage(new FileInfo(_filePath)))
+               public void AddResultsToSheet<T1, T2>(IEnumerable<T1> results1, IEnumerable<T2> results2, string sheetName)
         {
-            var worksheet = package.Workbook.Worksheets[sheetName] ?? package.Workbook.Worksheets.Add(sheetName);
-            var properties1 = typeof(T1).GetProperties()
-                .Where(p => p.PropertyType.IsValueType || p.PropertyType == typeof(string))
-                .ToArray();
-
-            var properties2 = typeof(T2).GetProperties()
-                .Where(p => p.PropertyType.IsValueType || p.PropertyType == typeof(string))
-                .ToArray();
-
-            // Add header row for results1
-            for (int i = 0; i < properties1.Length; i++)
+            using (var package = new ExcelPackage(new FileInfo(_filePath)))
             {
-                worksheet.Cells[1, i + 1].Value = properties1[i].Name;
-            }
+                var worksheet = package.Workbook.Worksheets[sheetName] ?? package.Workbook.Worksheets.Add(sheetName);
+                var properties1 = typeof(T1).GetProperties()
+                                            .Where(p => p.PropertyType.IsValueType || p.PropertyType == typeof(string))
+                                            .ToArray();
 
-            // Add header row for results2, starting after the last column of results1
-            for (int i = 0; i < properties2.Length; i++)
-            {
-                worksheet.Cells[1, properties1.Length + i + 1].Value = properties2[i].Name;
-            }
+                var properties2 = typeof(T2).GetProperties()
+                                            .Where(p => p.PropertyType.IsValueType || p.PropertyType == typeof(string))
+                                            .ToArray();
 
-            int row = 2;
-            int colOffset = properties1.Length + 1;
+                // Determine the last row with data to append new data without overwriting
+                int lastRow = worksheet.Dimension?.End.Row ?? 0;
+                int startRow = lastRow > 0 ? lastRow + 1 : 1;
 
-            // Add data rows for results1
-            foreach (var result in results1)
-            {
-                for (int col = 0; col < properties1.Length; col++)
+                // Add header row if not already present
+                if (startRow == 1)
                 {
-                    worksheet.Cells[row, col + 1].Value = properties1[col].GetValue(result);
+                    for (int i = 0; i < properties1.Length; i++)
+                    {
+                        worksheet.Cells[startRow, i + 1].Value = properties1[i].Name;
+                    }
+
+                    for (int i = 0; i < properties2.Length; i++)
+                    {
+                        worksheet.Cells[startRow, properties1.Length + i + 1].Value = properties2[i].Name;
+                    }
+
+                    startRow++;  // Move to the next row for data
                 }
 
-                row++;
-            }
+                int row = startRow;
 
-            row = 2; // Reset row counter for results2
-
-            // Add data rows for results2
-            foreach (var result in results2)
-            {
-                for (int col = 0; col < properties2.Length; col++)
+                // Add data rows for results1
+                foreach (var result in results1)
                 {
-                    worksheet.Cells[row, colOffset + col].Value = properties2[col].GetValue(result);
+                    for (int col = 0; col < properties1.Length; col++)
+                    {
+                        worksheet.Cells[row, col + 1].Value = properties1[col].GetValue(result);
+                    }
+                    row++;
                 }
 
-                row++;
+                row = startRow;  // Reset row counter for results2
+
+                // Add data rows for results2
+                foreach (var result in results2)
+                {
+                    for (int col = 0; col < properties2.Length; col++)
+                    {
+                        worksheet.Cells[row, properties1.Length + col + 1].Value = properties2[col].GetValue(result);
+                    }
+                    row++;
+                }
+
+                package.Save();
             }
-
-            package.Save();
         }
-    }
 
-    public void CreateGraph()
-    {
-        using (var package = new ExcelPackage(new FileInfo(_filePath)))
+        public void CreateGraph()
         {
-            var workbook = package.Workbook;
-            var worksheet = workbook.Worksheets[_sheetName] ?? workbook.Worksheets.Add(_sheetName);
+            using (var package = new ExcelPackage(new FileInfo(_filePath)))
+            {
+                var workbook = package.Workbook;
+                var worksheet = workbook.Worksheets[_sheetName] ?? workbook.Worksheets.Add(_sheetName);
 
-            // Define the range of data to be used for the chart
-            var dataSheet = workbook.Worksheets["CombinedPayments"];
+                // Define the range of data to be used for the chart
+                var dataSheet = workbook.Worksheets["CombinedPayments"];
 
-            // Create a new chart
-            var chart = worksheet.Drawings.AddChart("PaymentsChart", eChartType.Line) as ExcelLineChart;
+                // Create a new chart
+                var chart = worksheet.Drawings.AddChart("PaymentsChart", eChartType.Line) as ExcelLineChart;
 
-            // Set the title of the chart
-            chart.Title.Text = "Payable vs Receivable Payments";
+                // Set the title of the chart
+                chart.Title.Text = "Payable vs Receivable Payments";
 
-            // Set data series for Payable Payments
-            var payableSeries = chart.Series.Add(dataSheet.Cells["B2:B6"], dataSheet.Cells["A2:A6"]);
-            payableSeries.Header = "Payable Amount";
+                // Set data series for Payable Payments
+                var payableSeries = chart.Series.Add(dataSheet.Cells["B2:B6"], dataSheet.Cells["A2:A6"]);
+                payableSeries.Header = "Payable Amount";
 
-            // Set data series for Receivable Payments
-            var receivableSeries = chart.Series.Add(dataSheet.Cells["D2:D6"], dataSheet.Cells["C2:C6"]);
-            receivableSeries.Header = "Receivable Amount";
+                // Set data series for Receivable Payments
+                var receivableSeries = chart.Series.Add(dataSheet.Cells["D2:D6"], dataSheet.Cells["C2:C6"]);
+                receivableSeries.Header = "Receivable Amount";
 
-            // Set the position and size of the chart
-            chart.SetPosition(1, 0, 3, 0);
-            chart.SetSize(800, 600);
+                // Set the position and size of the chart
+                chart.SetPosition(1, 0, 3, 0);
+                chart.SetSize(800, 600);
 
-            package.Save();
-        }
+                package.Save();
+            }
 
         #region Charts Added
 
